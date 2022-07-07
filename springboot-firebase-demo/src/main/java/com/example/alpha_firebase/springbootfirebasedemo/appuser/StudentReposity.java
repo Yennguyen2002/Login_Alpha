d6660be39b396ea54903abc67bd2980febdb8444
Yennguyen2002 <yen.nguyen@geg.com.sg>
2022-07-07 14:51:02 +0700

    Initial commit

--- springboot-firebase-demo/src/main/java/com/example/alpha_firebase/springbootfirebasedemo/appuser/StudentReposity.java
+++ springboot-firebase-demo/src/main/java/com/example/alpha_firebase/springbootfirebasedemo/appuser/StudentReposity.java
@@ -1,10 +1,12 @@
 package com.example.alpha_firebase.springbootfirebasedemo.appuser;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Transactional(readOnly = true)
-public interface StudentReposity {
+public interface StudentReposity
+         extends JpaRepository<AppUser, String> {
     Optional<AppUser>findByEmail(String email);
 }
--- springboot-firebase-demo/src/main/java/com/example/alpha_firebase/springbootfirebasedemo/registration/RegistrationService.java
+++ springboot-firebase-demo/src/main/java/com/example/alpha_firebase/springbootfirebasedemo/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package com.example.alpha_firebase.springbootfirebasedemo.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return "works";
