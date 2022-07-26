4d7e5b70497b9b2e78d5213bddaf7e94d498bbfa
Yennguyen2002 <yen.nguyen@geg.com.sg>
2022-07-26 14:41:17 +0700

    commit1

--- springboot-firebase-demo/src/main/java/com/example/alpha_firebase/springbootfirebasedemo/registration/EmailValidation.java
+++ springboot-firebase-demo/src/main/java/com/example/alpha_firebase/springbootfirebasedemo/registration/EmailValidation.java
@@ -1,2 +1,14 @@
-package com.example.alpha_firebase.springbootfirebasedemo.registration;public class EmailValidation {
+package com.example.alpha_firebase.springbootfirebasedemo.registration;
+
+import org.springframework.stereotype.Service;
+
+import java.util.function.Predicate;
+
+@Service
+public class EmailValidation implements Predicate<String> {
+    @Override
+    public boolean test(String s) {
+        // Regex
+        return true;
+    }
 }
--- springboot-firebase-demo/src/main/java/com/example/alpha_firebase/springbootfirebasedemo/registration/RegistrationService.java
+++ springboot-firebase-demo/src/main/java/com/example/alpha_firebase/springbootfirebasedemo/registration/RegistrationService.java
@@ -1,10 +1,29 @@
 package com.example.alpha_firebase.springbootfirebasedemo.registration;
 
+import com.example.alpha_firebase.springbootfirebasedemo.appuser.AppUser;
+import com.example.alpha_firebase.springbootfirebasedemo.appuser.AppUserRole;
+import com.example.alpha_firebase.springbootfirebasedemo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
+    private EmailValidation emailValidation;
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidation.test(request.getEmail());
+        if(!isValidEmail){
+            throw new IllegalArgumentException("Email invalid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstname(),
+                        request.getLastname(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
